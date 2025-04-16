d713198c7fe38f99af0ece9467c84c0b08b8a947
2MJ <attahiruj@gmail.com>
2025-04-16 21:26:14 +0200

    refactor dataflow and update SSC_32U controller event handling

--- src/ssc32u_controller/ssc32u_controller/main.py
+++ src/ssc32u_controller/ssc32u_controller/main.py
@@ -23,7 +23,7 @@ def main():
 
     for event in node:
         if event["type"] == "INPUT":
-            if event["id"] == "TICK":
+            if event["id"] == "JOINTS":
                 print(
                     f"""Node received:
                 id: {event["id"]},
@@ -36,6 +36,21 @@ def main():
                 node.send_output(
                     output_id="my_output_id", data=pa.array([1, 2, 3]), metadata={}
                 )
+    # for event in node:
+    #     if event["type"] == "INPUT":
+    #         if event["id"] == "TICK":
+    #             print(
+    #                 f"""Node received:
+    #             id: {event["id"]},
+    #             value: {event["value"]},
+    #             metadata: {event["metadata"]}"""
+    #             )
+
+    #         elif event["id"] == "my_input_id":
+    #             # Warning: Make sure to add my_output_id and my_input_id within the dataflow.
+    #             node.send_output(
+    #                 output_id="my_output_id", data=pa.array([1, 2, 3]), metadata={}
+    #             )
 
 
 if __name__ == "__main__":
